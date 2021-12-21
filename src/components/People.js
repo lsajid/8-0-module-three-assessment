323d3ab52582d003b7f509aa68f6189367a2c714
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2021-12-21 14:54:22 -0500

    started people.js, but test fails to load people page

--- src/components/People.js
+++ src/components/People.js
@@ -2,10 +2,56 @@ import React, { Component } from 'react'
 import "./People.css"
 
 export class People extends Component {
+    constructor() {
+        super();
+    
+        this.state = {
+            userInput: "",
+            people:[],
+            searched:"", 
+        }
+    }
+
+    componentDidMount() {
+        this.handleFetch();
+    }
+    
+    handleUserInput = (event) => {
+        this.setState({
+            userInput: event.target.value 
+        })
+    }
+
+    handleFetch = () => {
+        fetch(`https://ghibliapi.herokuapp.com/people/`)
+        .then((res)=> res.json())
+        .then((data)=> {
+            this.setState({
+                people: data,
+            })
+        }).catch((err)=> console.log(err));
+    }
+    
     render() {
         return (
-            <div>
-                Poeple
+            <div className='people'>
+                <h1>Search for a Person</h1>
+                <div>
+                    <form onSubmit={""}>
+                        <input
+                            type="text"
+                            onChange={this.handleUserInput}
+                            name="userInput"
+                            id="userInput"
+                            value={this.state.userInput}
+                            placeholder='Find your Person'
+                        />
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+                <div className="person-data">
+                    Found 
+                </div>
             </div>
         )
     }
